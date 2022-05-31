2b19a212fc65ba50ff6683ef263f00d8e4d7609a
Hedda G <hedda.gressel@gmail.com>
2022-05-31 13:32:17 +0200

    b tree

--- B_tree.py
+++ B_tree.py
@@ -76,3 +76,53 @@ class Node:
         if self.parent is None:
             return self.split_no_parent()
         return self.split_with_parent()
+
+######---------------------------------------------------------------------------------
+##### B TREE:
+class BTree:
+
+    def __init__(self, split_threshold):
+        self.split_threshold = split_threshold
+        self.root = Node()
+        self.height = 0
+        self.size = 0
+
+
+    def __len__(self):
+        return self.size
+    
+    def _find_node(self, current_node, key):
+        if current_node.contains_key(key):
+            return current_node
+        if current_node.is_leaf():
+            return None
+        child_index = current_node.get_insert_index(key) 
+        return self._find_node(current_node.children[child_index], key)
+    
+    def contains(self, key):
+        node = self._find_node(self.root, key)
+        return not node is None
+
+    def get_value(self, key):
+        node = self._find_node(self.root, key)
+        if node is None:
+            return None
+        return node.get_value(key)
+
+    def _add(self, current_node, key, value):
+        if current_node.is_leaf(): 
+            current_node.insert_entry(key, value)
+        else:
+            child_index = current_node.get_insert_index(key)
+            self._add(current_node.children[child_index], key, value)
+        if len(current_node) > self.split_threshold:
+            parent = current_node.split()
+            if current_node == self.root:
+                self.root = parent
+                self.height += 1
+
+    def add(self, key, value):
+        self._add(self.root, key, value)
+        self.size += 1
+
+    
